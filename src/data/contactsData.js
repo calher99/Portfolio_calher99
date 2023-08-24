65dd0a9a57189ebb7a812f4eb25060e3e36d347f
calher99 <calher99@gmail.com>
2023-08-24 17:45:15 +0100

    update info

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
   email: "calher99@gmail.com",
   phone: "+34 696534215",
-  address: "Sydney, Australia (Arrival 20th Sept)",
+  address: "Sydney, Australia",
   github: "https://github.com/calher99",
   facebook: "https://www.facebook.com/carlos.alonsohernando",
   linkedIn: "https://www.linkedin.com/in/carlos-alonso-hernando-3a0242114/",
